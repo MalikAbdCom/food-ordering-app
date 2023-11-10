6a1cc8a5124f58edee2572d3a049ce5eb0acd522
malikabdcom <emailmalikabdcom@gmail.com>
2023-11-10 10:05:12 +0700

    add tailwind, icons, render nav, load data menu and renderit to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import NavBar from "./components/NavBar.jsx";
+import {ItemLists} from "./components/ItemLists.jsx";
+
 function App() {
-  return (
-    <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
-    </>
-  );
+    return (
+        <div className={'bg-slate-950'}>
+            <NavBar/>
+            <ItemLists/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ItemLists.jsx
@@ -0,0 +1,42 @@
+import {useEffect, useState} from "react";
+
+export function ItemLists() {
+
+    const [dataMakanan, setDataMakanan] = useState([]);
+
+    useEffect( () => {
+        const dapetinData = async ()=>{
+            try {
+                const dataUrl = await fetch('http://localhost:3000/meals')
+                const data = await dataUrl.json()
+                setDataMakanan(data)
+            } catch (error){
+                console.log(error)
+            }
+        }
+
+        dapetinData()
+    },[]);
+
+    return (
+        <div className={' grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-8 py-16 w-11/12 xl:w-full max-w-screen-xl mx-auto'}>
+            {
+                dataMakanan.map((menu)=>{
+                    return (
+                        <div className={'rounded-xl bg-slate-200 border border-slate-600 overflow-hidden'}>
+                            <img className={'h-[20rem] object-cover'} src={`http://localhost:3000/${menu.image}`} alt={menu.name} />
+                            <div className={'p-4'}>
+                                <div>
+                                    <h2 className={'font-bold text-xl'}>{menu.name}</h2>
+                                    <p className={'text-orange-700 font-semibold'}>${menu.price}</p>
+                                    <p className={'text-xs'}>{menu.description}</p>
+                                </div>
+                                <button>Beli sekarang</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
