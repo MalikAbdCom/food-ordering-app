2f78856f8b42585f14df7a5ecca291f182e1c53f
malikabdcom <emailmalikabdcom@gmail.com>
2023-11-15 10:01:17 +0700

    add useReducer stater

--- src/store/CardContext.jsx
+++ src/store/CardContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const CartContext = createContext({
   item: [],
@@ -6,7 +6,24 @@ const CartContext = createContext({
   removeItem: () => {},
 });
 
+function cartValuesReducerFn(state, action) {
+  if (action === "ADD_ITEM") {
+    // ADD ITEM
+    console.log("item added");
+  }
+  if (action === "REMOVE_ITEM") {
+    // remove item
+    console.log("item removed");
+  }
+
+  return state;
+}
+
 export const CartContextProvider = ({ children }) => {
+  const [cartValues, dispatchCartValues] = useReducer(cartValuesReducerFn, {
+    item: [],
+  });
+
   return (
     <CartContext.Provider value={"something"}>{children}</CartContext.Provider>
   );
